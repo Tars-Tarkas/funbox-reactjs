348ecfeb49e38aa071d1519399d86affa7ed5989
Молодык Андрей <tarstarkas00@mail.ru>
2023-02-03 17:16:58 +0700

    add style

--- src/CardSection.jsx
+++ src/CardSection.jsx
@@ -1,40 +1,40 @@
-import { Fragment } from 'react';
+import { Fragment, useState } from 'react';
 import './CardSection.scss';
 import CardItem from './components/CardItem/CardItem';
 
-const cardItem =[
+const cardItem=[
     {
-        id: 1,
+        "id": 1,
         "caption": "Сказочное заморское яство",
         "title": "Нямушка",
         "subtitle": "с фуа-гра",
         "description": "10 порций мышь в подарок",
-        "weight": "2,0",
-        "img": "/images/cat.png",
-        
+        "weight": "0,5",
+        "img": "/images/cat.png",        
+        "disabled": false        
     },
     {
-        id: 2,
+        "id": 2,
         "caption": "Сказочное заморское яство",
         "title": "Нямушка",
         "subtitle": "с фуа-гра",
-        "description": "10 порций мышь в подарок",
+        "description": "20 порций мышь в подарок",
         "weight": "2,0",
         "img": "/images/cat.png",
-        "disabled": true
+        "disabled": false
     },
     {
-        id: 3,
+        "id": 3,
         "caption": "Сказочное заморское яство",
         "title": "Нямушка",
         "subtitle": "с фуа-гра",
-        "description": "10 порций мышь в подарок",
+        "description": "30 порций мышь в подарок",
         "weight": "2,0",
         "img": "/images/cat.png",
-        "disabled": true
+        "disabled": false
     },
     {
-        id: 4,
+        "id": 4,
         "caption": "Сказочное заморское яство",
         "title": "Нямушка",
         "subtitle": "с фуа-гра",
@@ -45,28 +45,37 @@ const cardItem =[
     }
 ];
 
-export default function CardSection(){
+export default function CardSection(){   
+ 
+    const [addProduct, setAddProduct]=useState([]);
+
+    const HandleAddProduct=(id)=>{
+        const addItems=cardItem.filter(item=>item.id===id)
+        setAddProduct(addItems)
+        console.log(addItems)
+    }
+
     return(
         <div className='card__block'>
             <div className='container'>
-                <div className='card__inner'>
+                <div className='card__inner'>            
                     {cardItem.map((item)=>{
-                        return (
-                            <Fragment key={item.id}>
-                                <CardItem  
+                        return ( 
+                            <Fragment key={item.id}> 
+                                <CardItem
+                                HandleAddProduct={HandleAddProduct}
+                                item={CardItem}
+                                id={item.id}
                                 caption={item.caption}
                                 title={item.title}
                                 subtitle={item.subtitle}
                                 description={item.description}
                                 weight={item.weight}
-                                img={item.img}/>    
+                                img={item.img} 
+                                disabled={item.disabled}/>
                             </Fragment>
                         )
                     })}
-                    
-                    
-                    
-                   
                 </div>        
             </div>
         </div>
--- src/components/CardItem/CardItem.jsx
+++ src/components/CardItem/CardItem.jsx
@@ -1,22 +1,28 @@
+import { useState } from "react"
 import "./CardItem.scss"
 
-export default function Card({caption, title, subtitle, description, weight, img}){
+export default function Card({HandleAddProduct, caption, title, subtitle, description, weight, img, disabled}){
+    const [checked, setChecked]=useState(false)
 
-    const HandleChecked=(e)=>{
-        console.log(e.target.checked);
+    const HandleCheck=(e)=>{
+        e.preventDefault();
+        setChecked(!checked);       
     }
+
     return(
-        <div className="card">                    
-            <input type="checkbox" id="checkbox" className="card__checked" onClick={HandleChecked}/>
-            <label className="card__content" htmlFor="checkbox">
-                <label>{caption}</label>
-                <label>{title}</label>
-                <label>{subtitle}</label>
-                <p>{description}</p>
-                <img className="card__img" src={img} alt="Cat" />                     
-            </label> 
-                   
-            
-        </div>
+             <div className={checked ? 'card__checked' : 'card'}>
+                <input type="checkbox" id="checkbox" className="card__checkbox"/>
+                <label htmlFor="checkbox" className={disabled ? 'card__disabled' : 'card__content'}  onClick={e=>HandleCheck(e)}>  
+                    <div className="card__content__block">
+                        <span className="card__caption">{caption}</span>
+                        <span className="card__title">{title}</span>
+                        <span className="card__subtitle">{subtitle}</span>
+                        <p className="card__descriprion">{description}</p>
+                        <span className="card__weight">{weight} <span className="card__unit">кг</span> </span>
+                        <img className="card__img" src={img} alt="Cat" />                    
+                    </div>
+                </label>
+            </div>       
+           
     )
 }
